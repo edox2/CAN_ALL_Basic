b48d1bd66345cd1fbc2e0da9afcb5e4a2780dcb0
benxyz1 <benjamin.wieland@gmx.ch>
2022-03-16 22:04:48 +0100

    updated error code structure

--- Can-All/Logik/inc/Errors.h
+++ Can-All/Logik/inc/Errors.h
@@ -60,20 +60,93 @@ typedef enum
    JTAG_IWRITE_TIMEOUT,                // 35
    JTAG_WRITE_COMMAND_TIMEOUT,         // 36
    JTAG_READ_COMMAND_TIMEOUT,          // 37
-   HIGH_SIDE_SWITCH_A_ERROR = 100,  //Switch A indicates error
-   HIGH_SIDE_SWITCH_B_ERROR = 101, //Switch B indicates error
-   MAIN_BOARD_ERROR = 200,   // Startup/HW error (board is not working properly)
-   BENDER_ERROR = 201,     // error code by CAN? (warning, error, todo figure out codes)
-   INVERTOR_ERROR = 202,   // error code by CAN? (Voltage, temperature todo figure out codes)
-   INTERLOCK_ERROR = 203,  // InterlockSig high (loop is not closed) -> disable battery's
-   PUMP_ERROR = 204,       // Something with the Pump's is wrong
-   PUMP_BOARD_ERROR = 300,   // Startup/HW error (board is not working properly)
-   INVERTER_COM = 301,           // Inverter Temperature cannot be read
-   VACUUM_ERROR = 302,           // Vacuum cannot be build up
-   VACUUM_PUMP_ERROR = 303,   //Vacuum pump not responding
-   VACUUM_SENSOR_ERROR = 304,   //vacuum Sensor reading wrong
-   REV_HEATER_BOARD_ERROR = 400,   // Startup/HW error (board is not working properly)
-   HEATER_ERROR = 401,     // Melt Fuse is busted, Heater OFF
+   //CAN-ALL errors
+   // Xxxxxx Error Level 1-8: Level->decide reaction, 9: all Stop
+   // xXxxxx Board, 1: Main Controller,  2: Pumps, 3: RevHeater
+   // xxXXxx Type,
+   // xxxxXX, Number
+   ERROR_LEVEL_1 = 100000, // Error Level 1 ->
+   ERROR_LEVEL_2 = 200000, // Error Level 2 ->
+   ERROR_LEVEL_3 = 300000, // Error Level 3 ->
+   ERROR_LEVEL_4 = 400000, // Error Level 4 ->
+   ERROR_LEVEL_5 = 500000, // Error Level 5 ->
+   ERROR_LEVEL_6 = 600000, // Error Level 6 ->
+   ERROR_LEVEL_7 = 700000, // Error Level 7 ->
+   ERROR_LEVEL_8 = 800000, // Error Level 8 ->
+   ERROR_LEVEL_FATAL = 900000, // Error Level 9 -> Fatal Error, all stop
+
+   //Board Definition
+   BOARD_MAIN_CONTROLLER = 100000,
+   BOARD_PUMP = 200000,
+   BOARD_REV_HEATER = 300000,
+
+   //Type Definition
+   TYPE_GENERIC = 100,
+   TYPE_BOARD = 200,
+   TYPE_INVERTER = 300,
+   TYPE_BATTERY = 400,
+   TYPE_BENDER = 500,
+   TYPE_COMM = 600,
+   TYPE_GPIO = 700,
+   TYPE_ADC = 800,
+   TYPE_HSS = 900,
+   TYPE_HW = 1000,
+
+
+
+   //Board HW errors
+   BOARD_ERROR_GENERIC = TYPE_BOARD + 0, // undefined board error
+   BOARD_HSS_A_ERROR = TYPE_BOARD + TYPE_HSS + 1,  // Switch A indicates error
+   BOARD_HSS_B_ERROR = TYPE_BOARD + TYPE_HSS + 2 , // Switch B indicates error
+
+   //Inverter errors
+   INVERTOR_ERROR_GENERIC = TYPE_INVERTER + 0,   // undefined inverter error
+   INVERTOR_ERROR_CAN = TYPE_INVERTER + 1,  //
+   INVERTER_ERROR_TEMPERATURE = TYPE_INVERTER + 2, //
+   INVERTER_ERROR_BATTERY_LEVEL = TYPE_INVERTER + 3, //
+   INVERTER_ERROR_PARAMETER = TYPE_INVERTER + 4, //
+   //INVERTER_ERROR_ = 000105, //
+   //INVERTER_ERROR_ = 000106, //
+   //INVERTER_ERROR_ = 000107, //
+   //INVERTER_ERROR_ = 000108, //
+   //INVERTER_ERROR_ = 000109, //
+   //todo figure out codes/errors
+
+   //Battery Errors
+   BATTERY_ERROR_GENERIC = TYPE_BATTERY + 0, //
+   BATTERY_ERROR_UNDER_VOLTAGE = TYPE_BATTERY + 1, //
+   BATTERY_ERROR_TEMPERATURE = TYPE_BATTERY + 2, //
+   //BATTERY_ERROR_ = TYPE_BATTERY + , //
+   //BATTERY_ERROR_ = TYPE_BATTERY + , //
+   //BATTERY_ERROR_ = TYPE_BATTERY + , //
+   //BATTERY_ERROR_ = TYPE_BATTERY + , //
+   //BATTERY_ERROR_ = TYPE_BATTERY + , //
+   //BATTERY_ERROR_ = TYPE_BATTERY + , //
+
+   //Bender Errors
+   BENDER_ERROR_GENERIC = TYPE_BENDER + 0,
+   BENDER_ERROR_WARNING = TYPE_BENDER + 1, //  Insulation Warning
+   BENDER_ERROR_ERROR = TYPE_BENDER + 2, // Insulation Error
+   //BENDER_ERROR_ = 000303, //
+   //BENDER_ERROR_ = 000304, //
+   //BENDER_ERROR_ = 000305, //
+   //BENDER_ERROR_ = 000306, //
+   //BENDER_ERROR_ = 000307, //
+   //BENDER_ERROR_ = 000308, //
+   //BENDER_ERROR_ = 000309, //
+   //todo figure out codes/errors
+
+   //Main Controller Board
+   BOARD_INTERLOCK_ERROR = BOARD_MAIN_CONTROLLER + TYPE_GPIO + 1,  // InterlockSig high (loop is not closed)
+   BOARD_PUMPS_EROR = BOARD_MAIN_CONTROLLER + TYPE_HW + 2,  // one of the pumps is not wirking (signal from Pump-Board)
+   //Pump Board
+   BOARD_INVERTER_COM =  BOARD_PUMP + TYPE_COMM + 1, // Inverter Temperature cannot be read
+   BOARD_VACUUM_PUMP_ERROR = BOARD_PUMP + TYPE_HSS + 2, // Something with the Pump's is wrong
+   BOARD_VACUUM_SYSTEM_ERROR = BOARD_PUMP + TYPE_HW + 3 , // Vacuum cannot be build up
+   BOARD_VACUUM_SENSOR_ERROR = BOARD_PUMP + TYPE_ADC + 2,   //vacuum Sensor reading wrong
+
+   //Reverse Light, Heater Melting fuse BOARD
+   HEATER_FUSE_ERROR = BOARD_REV_HEATER + TYPE_HW + 1,     // Melt Fuse is busted, Heater OFF
 }ERROR_RECORD;
 
 //typedef struct
